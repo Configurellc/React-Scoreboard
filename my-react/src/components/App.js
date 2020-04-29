b47b23682827fa74a8a7b9c314791cf755251eb3
configurellc <configurellc@gmail.com>
2020-04-28 21:16:24 -0400

    Seperated the components located in index.js into a seperate files in component foler

---
+++ my-react/src/components/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Header from './Header';
+import Player from './Player';
+
+
+class App extends React.Component {
+    state = {
+        players: [
+            {
+                name: "Guil",
+                id: 1
+              },
+              {
+                name: "Treasure",
+                id: 2
+              },
+              {
+                name: "Ashley",
+                id: 3
+              },
+              {
+                name: "James",
+                id: 4
+              }
+        ]
+    }
+
+    handleRemovePlayer = (id) => {
+        this.setState( prevState => {
+            return {
+                players: prevState.players.filter(player => player.id !== id)
+            }
+        })
+    }
+    
+    render() {
+        return (
+            <div className="scoreboard">
+                <Header title="Scoreboard" 
+                totalPlayers={this.state.players.length}        
+                />
+
+                {/*Players list*/}
+                {this.state.players.map( player =>
+                    <Player 
+                    playerName={player.name} 
+                    id={player.id}
+                    key={player.id.toString()}
+                    removePlayer={this.handleRemovePlayer}
+                    />
+                )}
+
+            </div>
+        )
+
+    }
+
+
+}
+
+export default App;

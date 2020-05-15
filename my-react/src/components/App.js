75528d392e7a8d0b9877e40be55040ed60e7ec44
configurellc <configurellc@gmail.com>
2020-05-15 02:02:51 -0400

    Moved the "score" state in counter.js to the App component to be accessed by other components

--- my-react/src/components/App.js
+++ my-react/src/components/App.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
 
 
-class App extends React.Component {
+class App extends Component {
     state = {
         players: [
             {
                 name: "Guil",
-                id: 1
+                id: 1,
+                score: 0
               },
               {
                 name: "Treasure",
-                id: 2
+                id: 2,
+                score: 0
               },
               {
                 name: "Ashley",
-                id: 3
+                id: 3,
+                score: 0
               },
               {
                 name: "James",
-                id: 4
+                id: 4,
+                score: 0
               }
         ]
+    }
+
+    handleScorechange = (delta) => {
+    
+        // this.setState( prevState => {
+        //     return {
+        //         score: prevState.score +1
+        //     }
+        // });  
+        console.log( delta )      
     }
 
     handleRemovePlayer = (id) => {
@@ -44,8 +58,10 @@ class App extends React.Component {
                 {this.state.players.map( player =>
                     <Player 
                     playerName={player.name} 
+                    score={player.score}
                     id={player.id}
                     key={player.id.toString()}
+                    changeScore={this.handleScorechange}
                     removePlayer={this.handleRemovePlayer}
                     />
                 )}

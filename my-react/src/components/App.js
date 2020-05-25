b415ef0c25a84fe1919368ddaf7bc016a5384040
configurellc <configurellc@gmail.com>
2020-05-24 23:02:52 -0400

    updated app component

--- my-react/src/components/App.js
+++ my-react/src/components/App.js
@@ -49,15 +49,18 @@ class App extends Component {
     }
 
     handleAddPlayer = (name) => {
-        this.setState({
-            players: [
-            ...this.state.players, 
-            {
-                name,
-                score: 0,
-                id: this.prevPlayerId += 1
+        console.log( ...this.state.players )
+        this.setState( prevState => {
+            return {
+                players: [
+                ...prevState.players, 
+                {
+                    name,
+                    score: 0,
+                    id: this.prevPlayerId += 1
+                }
+                ]
             }
-            ]
         });
     }
 

d671421235e4b9f389e55711b70cb708e4c8add2
configurellc <configurellc@gmail.com>
2020-06-03 00:47:30 -0400

    added Stopwatch component to header

--- my-react/src/components/Header.js
+++ my-react/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stats from './stats';
+import Stopwatch from './Stopwatch';
 
 const Header = (props) => {
     console.log(props )
@@ -9,6 +10,7 @@ const Header = (props) => {
                 players={props.players}
             />
             <h1>{props.title}</h1>
+            <Stopwatch />
         </header>
     );
 }
